0d57e8cbbd30d7b9e0c1f477e85eafe77a2bbb02
KaimuSeino <skim07a19yki@icloud.com>
2025-09-06 11:30:33 +0900

    テストのリクエスト処理にContent-Typeを明示的に指定しないとHTMLを出力しようとするので修正。

--- test/controllers/v1/drawings_controller_test.rb
+++ test/controllers/v1/drawings_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
-class Api::V1::DrawingsControllerTest < ActionDispatch::IntegrationTest
+class V1::DrawingsControllerTest < ActionDispatch::IntegrationTest
   test "#index MNISTデータの一覧情報を取得できる" do
-    get api_v1_drawings_url
+    get v1_drawings_url, as: :json
     assert_response :success
   end
 
@@ -10,7 +10,7 @@ class Api::V1::DrawingsControllerTest < ActionDispatch::IntegrationTest
     file = fixture_file_upload("test_drawing.png", "image/png")
 
     assert_difference("Drawing.count", 1) do
-      post api_v1_drawings_url, params: { label: 2, image: file }
+      post v1_drawings_url, params: { label: 2, image: file }, as: :json
     end
 
     assert_response :created
