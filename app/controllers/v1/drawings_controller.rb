968f7d5ea7e0527d741c774cbc1c1b354f6eee41
KaimuSeino <skim07a19yki@icloud.com>
2025-09-07 22:08:20 +0900

    インスタンス変数に修正

--- app/controllers/v1/drawings_controller.rb
+++ app/controllers/v1/drawings_controller.rb
@@ -1,26 +1,26 @@
 class V1::DrawingsController < ApplicationController
   def index
     user = User.first
-    drawings = user.drawings.order(created_at: :desc)
-    render :index, locals: { drawings: drawings }
+    @drawings = user.drawings.order(created_at: :desc)
   end
 
   def create
     user = User.first
 
-    drawing = user.drawings.build(label: drawing_params[:label])
+    @drawing = user.drawings.build(label: drawing_params[:label])
     if drawing_params[:image].present?
-      drawing.image.attach(drawing_params[:image])
+      @drawing.image.attach(drawing_params[:image])
     end
 
-    if drawing.save
-      render :show, status: :created, locals: { drawing: drawing }
+    if @drawing.save
+      render :show, status: :created, locals: { drawing: @drawing }
     else
-      render :errors, status: :unprocessable_entity, locals: { record: drawing }
+      render :errors, status: :unprocessable_entity, locals: { record: @drawing }
     end
   end
 
   def show
+    @drawing = Drawing.find(params[:id])
   end
 
   private
