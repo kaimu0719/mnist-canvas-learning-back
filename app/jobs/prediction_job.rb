05132595d1831e989209366a1e028928982b98a5
KaimuSeino <skim07a19yki@icloud.com>
2025-09-16 00:01:43 +0900

    FastAPIに推論リクエストを送るようにJobを修正

--- app/jobs/prediction_job.rb
+++ app/jobs/prediction_job.rb
@@ -1,18 +1,49 @@
 # 推論用のリクエストをNuxt側から受け取って、FastAPI側に推論のリクエストを送る処理。
-# RailsのActionJobで非同期に実行して推論が完了したら推論結果をprediction_logsテーブルに保存する。
+# 推論が完了したら推論結果をprediction_logsテーブルに保存する。
 class PredictionJob < ApplicationJob
-  queue_as :default
+  include Rails.application.routes.url_helpers
 
+  queue_as :default
+  
   def perform(job_id:)
     require "net/http"
     require "uri"
-
-    uri = URI.parse("http://host.docker.internal:8000")
-    response = Net::HTTP.get_response(uri)
-
-    data = JSON.parse(response.body)
+    require "json"
 
     prediction_log = PredictionLog.find_by!(job_id: job_id)
+    drawing = prediction_log.drawing
+
+    if drawing.image.attached? == false
+      prediction_log.update(status: "failed")
+      Rails.logger.error("[PredictionJob] job_id=#{job_id} error=NoImageAttached")
+      return
+    end
+
+    app_uri = URI.parse("http://localhost:3000")
+
+    host_with_port =
+      if app_uri.port && ![80, 443].include?(app_uri.port)
+        "#{app_uri.host}:#{app_uri.port}"
+      else
+        app_uri.host
+      end
+    
+    # 署名付きURL
+    image_url = rails_blob_url(drawing.image, disposition: "attachment", host: host_with_port, protocol: app_uri.scheme)
+
+    uri = URI.join("http://host.docker.internal:8000", "/predict")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.open_timeout = 10
+    http.read_timeout = 30
+    request = Net::HTTP::Post.new(uri.request_uri, { "Content-Type" => "application/json" })
+    request.body = {
+      image_url: image_url,
+    }.to_json
+
+    Rails.logger.info("[PredictionJob] POST #{uri} body=#{request.body}")
+    response = http.request(request)
+    Rails.logger.info("[PredictionJob] RES code=#{response.code} len=#{response.body&.bytesize}")
+    data = JSON.parse(response.body)
 
     prediction_log.update!(answer: data["answer"], status: "completed")
 
