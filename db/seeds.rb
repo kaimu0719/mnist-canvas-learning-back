83aae6461009d5ea81c1371b461bb454a4e2ca4e
KaimuSeino <skim07a19yki@icloud.com>
2025-09-22 22:02:08 +0900

    シードデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# パスワードハッシュを自動生成するため has_secure_password を前提にしている
+User.find_or_create_by!(email_address: "test@example.com") do |user|
+  user.password = "123456"
+end
